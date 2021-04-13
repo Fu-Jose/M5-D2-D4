1872ee3d514ce63ad364ffc7ecf3dd54cf1e4717
bioback <78535998+bioback@users.noreply.github.com>
2021-04-13 18:04:10 +0200

    fixed get, post, put ,delete

--- src/index.js
+++ src/index.js
@@ -17,19 +17,44 @@ router.get("/", (req, res) => {
 })
 
 router.get("/:id", (req, res) => {
-
+    console.log("GET ID OK")
+    const fileAsABuffer = fs.readFileSync(studentsJSONPath)
+    const fileAsAString = fileAsABuffer.toString()
+    const students = JSON.parse(fileAsAString)
+    const student = students.find(s => s.ID === req.params.id)
+    res.send(student)
 })
 
 router.post("/", (req, res) => {
-
+    const fileAsABuffer = fs.readFileSync(studentsJSONPath)
+    const fileAsAString = fileAsABuffer.toString()
+    const students = JSON.parse(fileAsAString)
+    const newStudent = req.body
+    newStudent.ID = uniqid()
+    students.push(newStudent)
+    fs.writeFileSync(studentsJSONPath, JSON.stringify(students))
+    res.status(201).send({ id: newStudent.ID })
 })
 
 router.put("/:id", (req, res) => {
-    
+    const fileAsABuffer = fs.readFileSync(studentsJSONPath)
+    const fileAsAString = fileAsABuffer.toString()
+    const students = JSON.parse(fileAsAString)
+    const newStudent = students.filter(s => s.ID !== req.params.id)
+    const modifiedStudent = req.body
+    modifiedStudent.ID = req.params.id
+    newStudent.push(modifiedStudent)
+    fs.writeFileSync(studentsJSONPath, JSON.stringify(newStudent))
+    res.send("IS THIS WORKING?")
 })
 
 router.delete("/:id", (req, res) => {
-    
+    const fileAsABuffer = fs.readFileSync(studentsJSONPath)
+    const fileAsAString = fileAsABuffer.toString()
+    const students = JSON.parse(fileAsAString)
+    const newStudent = students.filter(s=>s.ID !== req.params.id)
+    fs.writeFileSync(studentsJSONPath, JSON.stringify(newStudent))
+    res.send("DELETE?")
 })
 
 export default router
